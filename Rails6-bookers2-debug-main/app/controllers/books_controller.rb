34bfe3c4a5824c0ab57f23addf04128cbd9071eb
IkkanWatanabe <110508666+ichi-7@users.noreply.github.com>
2022-09-19 03:27:35 +0000

    [save] kadai-2

--- Rails6-bookers2-debug-main/app/controllers/books_controller.rb
+++ Rails6-bookers2-debug-main/app/controllers/books_controller.rb
@@ -1,4 +1,8 @@
 class BooksController < ApplicationController
+  
+  def new
+    @book = Book.new
+  end
 
   def show
     @book = Book.find(params[:id])
@@ -6,15 +10,16 @@ class BooksController < ApplicationController
 
   def index
     @books = Book.all
+    @book = Book.new
   end
 
   def create
+    @books = Book.all
     @book = Book.new(book_params)
     @book.user_id = current_user.id
     if @book.save
       redirect_to book_path(@book), notice: "You have created book successfully."
     else
-      @books = Book.all
       render 'index'
     end
   end
@@ -41,6 +46,6 @@ class BooksController < ApplicationController
   private
 
   def book_params
-    params.require(:book).permit(:title)
+    params.require(:book).permit(:title,:body)
   end
 end
